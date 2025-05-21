44d73fa7b19b6f63e3cdbc79f7824e26679021cd
mahdi-islam-pranto <mahdiprantoblog@gmail.com>
2025-05-21 16:01:30 +0600

    connect browser to websocket server

---
+++ websocket_example/connect_browser/show_time.js
@@ -0,0 +1,21 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const messages = document.createElement("ul");
+  document.body.appendChild(messages);
+
+  const websocket = new WebSocket("ws://localhost:5678/");
+  websocket.onmessage = ({ data }) => {
+    const message = document.createElement("li");
+    const content = document.createTextNode(data);
+    message.appendChild(content);
+    messages.appendChild(message);
+  };
+
+
+  // handle stop button
+document.getElementById("stop").addEventListener("click", () => {
+  // close the websocket connection
+  websocket.close();
+  
+});
+});
+
